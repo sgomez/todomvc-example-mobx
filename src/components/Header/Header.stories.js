3737531b745265576e559275b74db841959d972e
Sergio Gómez <sergio@uco.es>
2017-02-21 11:24:00 +0100

    Borrado decorator innecesario

--- src/components/Header/Header.stories.js
+++ src/components/Header/Header.stories.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { action, storiesOf } from '@kadira/storybook';
 
 import { Header } from './Header';
-import StoreDecorator from '../../../stories/decorators/StoreDecorator';
 
 import 'todomvc-app-css/index.css';
 import TodoModel from "../../models/TodoModel/TodoModel";
@@ -15,7 +14,6 @@ const mockupHandeSaveTodo = (text) => {
 };
 
 storiesOf('Header', module)
-    .addDecorator(StoreDecorator)
     .add('Show header', () => (
         <Header addTodo={mockupHandeSaveTodo} todo={todo} />
     ))
